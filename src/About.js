cbe7bfdb36c6aba03f8d4d37b63b9b3ec4f3745b
Nataly <nataly@MacBook-Air-Nataly.local>
2022-10-20 16:52:26 +0300

    исправила img

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 function About() {
     return (
         <div className="container">
-            <img className="fotoAbout" src="https://img.freepik.com/free-photo/chair-and-table-in-yellow-living-room-with-copy-space_43614-859.jpg?w=1060&t=st=1666172043~exp=1666172643~hmac=6d99004bdf91bc8363376aa485cd8eca646ad843a0761b4528280bac119c1e8f" width="400px"/>
+            <img className="fotoAbout" src="https://img.freepik.com/free-photo/chair-and-table-in-yellow-living-room-with-copy-space_43614-859.jpg?w=1060&t=st=1666172043~exp=1666172643~hmac=6d99004bdf91bc8363376aa485cd8eca646ad843a0761b4528280bac119c1e8f" width="400px" alt="pict"/>
             <p className="textAbout">
             The desire to win back a place for harmony and peace from the chaos of life is the main idea that pervades all our projects. We strive to rethink the space for the content and perfection of each of its elements. Here you will not find excessive embellishment and avoidance of meaning, from function for the sake of form.
             Creating a project begins with working with light. It is light that sets the basic forms of space, reveals textures and creates an atmosphere. Other important elements are laconic volumes, natural materials, clear geometric contours of objects, freedom and flexibility of composition. We call this style cozy minimalism.
--- src/Interitem.js
+++ src/Interitem.js
@@ -10,10 +10,10 @@ function Interitem({anyItem}) {
     return (
         <div className="products">
             {anyItem.map(item => {
-            const {id, key, name, description, image, showMore} = item;
+            const {id, name, description, image, showMore} = item;
             return (
                 <div key = {id} className="product-card">
-                    <img src={image} width="400px" height="300px"/>
+                    <img src={image} width="400px" height="300px" alt="interior"/>
                     <h2>{name}</h2>
                     <p className="descr">{showMore ? description : description.substring(0, 100) + "....."}
                     <button className="showButton" onClick={() => sck(item)}>{showMore ? "Show less" : "Show more"}</button>
